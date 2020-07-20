f59cb15e35452e58fd98c1e13a593883cf2c9b35
lupeng@work <1911986273@qq.com>
2020-07-20 17:44:43 +0800

    update

--- miniprogram/pages/index.js
+++ miniprogram/pages/index.js
@@ -5,6 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
+    lists: [{
+      uuid:'nyEyQUVfj2H3EjSrbMjLXP',
+      cate_icon: 'food',
+      price: '32',
+      date: '2020-07-20',
+      tags: '生活,小宝',
+      bz: '米粉updateupdate',
+      is_auto: true
+    }],
+    default_content_title: '当月支出',
     navigationBar: {
       title: "默认账本",
       loading: false,
@@ -13,7 +23,7 @@ Page({
       show: true,
       animated: false
     },
-    list: getApp().globalData.tabbar_list,
+    tabbar_list: getApp().globalData.tabbar_list,
     cell: {
       icon: '/images/category/food.png',
       title: '2020-04-22'
@@ -26,6 +36,11 @@ Page({
   },
   tabChange(e){
     console.log(e);
+  },
+  add_cost(e){
+    wx.navigateTo({
+      url: './index/add/index',
+    })
   },
   /**
    * 生命周期函数--监听页面加载
